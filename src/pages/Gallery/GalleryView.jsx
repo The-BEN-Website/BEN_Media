c8932fb1a0835bba7cc85eda75685184addd8af0
oghene-ella <oghenekaro57@gmail.com>
2024-01-23 19:55:23 -0600

    made  some changes to the sub route for the teachings and gallery page

--- src/pages/Gallery/GalleryView.jsx
+++ src/pages/Gallery/GalleryView.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ImageView = () => {
-  const { imageId } = useParams();
-  const [image, setImage] = useState(null);
+  const { id } = useParams();
+  const [image, setImage] = useState([]);
+
 
   useEffect(() => {
     const fetchImage = async () => {
       try {
         // Use a separate variable to avoid closure issues
-        const id = imageId;
-        const response = await axios.get(`https://picsum.photos/id/${id}/info`);
+        const Id = id;
+        const response = await axios.get(`https://picsum.photos/id/${Id}/info`);
         setImage(response.data);
       } catch (error) {
         console.error('Error fetching image details:', error);
@@ -19,18 +20,29 @@ const ImageView = () => {
     };
 
     fetchImage();
-  }, [imageId]);
+  }, [id]);
 
   if (!image) {
     return <div>Loading...</div>;
   }
 
+
   return (
-    <div>
-      <h2>Image Detail</h2>
-      <img src={image.download_url} alt={image.author} style={{ maxWidth: '400px' }} />
-      <p>Author: {image.author}</p>
-      <p>Description: {image.description}</p>
+    <div key={image.id} className='flex flex-col items-center mb-10 mt-10'>
+      <section className='w-11/12 flex flex-col gap-5'>
+        <span className='flex justify-between'>
+          <Link to={`/gallery`} className='underline text-base cursor-pointer'>
+            Back
+          </Link>
+          <h2 className='font-semibold text-3xl'>Title!</h2>
+        </span>
+      
+        <img src={image.download_url} alt={image.author} className='w-full rounded-md' />
+        <p className='text-gray-500'>
+          <b className='text-my-black font-semibold text-base'> Description: </b> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae vel nobis a explicabo quo obcaecati laboriosam, reprehenderit beatae eum rerum et voluptatum molestias nemo eveniet impedit? Temporibus quos quo eos aperiam. Nihil, officia velit nulla veniam id architecto sint? Laudantium, ab earum laboriosam quidem itaque sunt? Animi quisquam quis neque!</p>
+
+        </section>
     </div>
   );
 };
---
+++ src/pages/Teachings/TeachingViews.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useParams } from 'react-router-dom';
+
+const ImageView = () => {
+  const { id } = useParams();
+  const [image, setImage] = useState([]);
+
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        // Use a separate variable to avoid closure issues
+        const Id = id;
+        const response = await axios.get(`https://picsum.photos/id/${Id}/info`);
+        setImage(response.data);
+      } catch (error) {
+        console.error('Error fetching image details:', error);
+      }
+    };
+
+    fetchImage();
+  }, [id]);
+
+  if (!image) {
+    return <div>Loading...</div>;
+  }
+
+
+  return (
+    <div key={image.id} className='flex flex-col items-center mb-10 mt-10'>
+      <section className='w-11/12 flex flex-col gap-5'>
+      <Link to={`/teachings`} className='underline text-base cursor-pointer'>
+            Back
+        </Link>
+      
+        <img src={image.download_url} alt={image.author} className='w-full rounded-md' />
+
+        <h2 className='text-3xl font-bold'>Title</h2>
+        <p className='text-gray-500'>
+          <b className='text-my-black font-semibold text-base'> Description: </b> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae vel nobis a explicabo quo obcaecati laboriosam, reprehenderit beatae eum rerum et voluptatum molestias nemo eveniet impedit? Temporibus quos quo eos aperiam. Nihil, officia velit nulla veniam id architecto sint? Laudantium, ab earum laboriosam quidem itaque sunt? Animi quisquam quis neque!
+        </p>
+
+        <section className='flex justify-between gap-5 w-full border border-black h-10'>
+            <span className='border border-red-600 w-1/2'></span>
+            <span className='border border-blue-700 w-1/2'></span>
+        </section>
+          <section className='flex items-center gap-3'>
+                <img src={image.download_url} className='w-10 h-10 rounded-full' />
+                <span className='flex flex-col gap-1'>
+                  <p className='text-base font-semibold text-my-black' >{image.author}</p>
+                  <p className='text-xs text-gray-500'>Dec 24th, 2023</p>
+                </span>
+            </section>
+
+        </section>
+    </div>
+  );
+};
+
+export default ImageView;
