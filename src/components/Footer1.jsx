c4f2c20cbdd8fab2a7f8dcb3ad6fa7248732aaee
Emmanuella Oghenekaro <oghenekaro57@gmail.com>
2026-09-21 17:16:39 -0400

    Update footer to display current year dynamically

--- src/components/Footer1.jsx
+++ src/components/Footer1.jsx
@@ -3,6 +3,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className='bg-footer-background h-[8rem] flex flex-col'>
         <div className='m-auto text-center text-black flex flex-col gap-y-3'>
@@ -11,7 +12,7 @@ const Footer = () => {
             <a href="#" className='p-3 bg-white rounded-full'><InstagramIcon fontSize="medium" /></a>
             <a href="#" className='p-3 bg-white rounded-full'><YouTubeIcon fontSize="medium" /></a>
         </span>
-        <p className='font-semibold text-xl text-white font-my_font'>Believers Equipping Network &copy; 2023</p>
+        <p className='font-semibold text-xl text-white font-my_font'>Believers Equipping Network &copy; {currentYear}</p>
         </div>
     </div>
   )
